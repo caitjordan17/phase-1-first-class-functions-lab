72c4181001c229bde26210b48ba99053886515df
Caitlin Jordan <jordan.cait17@gmail.com>
2023-01-21 16:49:23 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    const halfArray = [...array]
+    return halfArray.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(array){
+    const halfArray = [...array]
+    return halfArray.slice(2,5);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+    let multiply = (fare) => fare * number
+    return multiply;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (array, fn){
+    return fn([...array]);
+}
